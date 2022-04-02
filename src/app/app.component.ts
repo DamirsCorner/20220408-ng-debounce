b89f9df2ffb917feeeade7c37f8c5cbf498e5f78
Damir Arh <damir.arh@gmail.com>
2022-04-02 10:50:55 +0200

    Debounce implementation with RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  Subject,
+  Subscription,
+  switchMap,
+} from 'rxjs';
 import { SearchService } from './search.service';
 
 @Component({
@@ -6,15 +13,30 @@ import { SearchService } from './search.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private readonly searchSubject = new Subject<string | undefined>();
+  private searchSubscription?: Subscription;
+
   public searchResults: string[] = [];
 
   constructor(private searchService: SearchService) {}
 
+  public ngOnInit(): void {
+    this.searchSubscription = this.searchSubject
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        switchMap((searchQuery) => this.searchService.search(searchQuery))
+      )
+      .subscribe((results) => (this.searchResults = results));
+  }
+
+  public ngOnDestroy(): void {
+    this.searchSubscription?.unsubscribe();
+  }
+
   public onSearchQueryInput(event: Event): void {
     const searchQuery = (event.target as HTMLInputElement).value;
-    this.searchService
-      .search(searchQuery)
-      .subscribe((results) => (this.searchResults = results));
+    this.searchSubject.next(searchQuery?.trim());
   }
 }
