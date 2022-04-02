c81bbd4327609edd5fe1fbd4c6643edd146a2617
Damir Arh <damir.arh@gmail.com>
2022-04-02 10:11:16 +0200

    Simple search implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { SearchService } from './search.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-debounce';
+  public searchResults: string[] = [];
+
+  constructor(private searchService: SearchService) {}
+
+  public onSearchQueryInput(event: Event): void {
+    const searchQuery = (event.target as HTMLInputElement).value;
+    this.searchService
+      .search(searchQuery)
+      .subscribe((results) => (this.searchResults = results));
+  }
 }
